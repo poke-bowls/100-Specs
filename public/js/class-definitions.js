2d830b1f8b8e9f16b308f759326c63fbe1b4f6c4
Micah Koki <micah.koki@gmail.com>
2015-12-16 15:37:16 -0800

    WarmBlooded or nah?

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1152,6 +1152,17 @@ var dinner = new Meal( 'fish, vegetables' );
  *
  */
 
+Animal.prototype.isWarmBlooded = function() {
+  if ( this.species === "Fish" ) {
+    return false;
+  } else if ( this.species === "Monkey" ){
+    return true;
+  } else if ( this.species === "Bird" ) {
+    return true;
+  } else {
+    return "Could not determine if warm-blooded";
+  }
+};
 
 /* Step 82
  *
