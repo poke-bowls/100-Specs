db2d2da6a4093bb01a18408d044da0d6e9445602
Micah Koki <micah.koki@gmail.com>
2015-12-16 03:11:52 -0800

    Vehicle

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -978,6 +978,10 @@ function Animal( species, gender ) {
  * @param {string} model The vehicle's model
  */
 
+function Vehicle( make, model ) {
+  this.make = make;
+  this.model = model;
+}
 
 /**
  * Step 52
