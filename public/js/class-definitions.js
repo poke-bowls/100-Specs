958175e98e327ce5abd32002d7c40ad525717832
Micah Koki <micah.koki@gmail.com>
2015-12-15 18:02:43 -0800

    Stacking cheddar cuz it's in my DNA

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -210,6 +210,7 @@ var linuxFlavors = ["Gentoo", "Fedora", "Debian", "Slackware", "Red Hat", "Biebe
  *
  */
 
+var DNA = ["nucleatides", "guanine", "adenine", "thymine", "cytosine"];
 
 /* Step 16
  *
