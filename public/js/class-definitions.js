e9c3b399b734b5ea2aca299fd976030f9d0d7eb5
Micah Koki <micah.koki@gmail.com>
2015-12-15 18:07:23 -0800

    comin up with the same crew from back in the day, the only cash lost was whatever this laptopCosts

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -224,6 +224,12 @@ var DNA = ["nucleatides", "guanine", "adenine", "thymine", "cytosine"];
  *
  */
 
+var laptopCosts = {
+  MacBook: 1500,
+  Alienware: 2500,
+  HP: 499,
+  Surface: 320
+};
 
 /* Step 17
  *
