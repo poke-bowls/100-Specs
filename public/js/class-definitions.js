3581e3dbf30986d7cb36fe9b309caab2df601e79
Micah Koki <micah.koki@gmail.com>
2015-12-16 23:21:28 -0800

    Drive

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1172,6 +1172,13 @@ Animal.prototype.isWarmBlooded = function() {
  *
  */
 
+Vehicle.prototype.drive = function( streetName ) {
+  if ( typeof streetName === 'string' && streetName.length !== 0 ) {
+    return "Driving on " + streetName;
+  } else {
+    return "Driving forward";
+  }
+};
 
  /* Step 83
  *
